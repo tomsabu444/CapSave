d2f1c7bd14ff06c703dfba0484e7e7c25941c44d
TOM SABU <tomsabu0000@gmail.com>
2025-05-01 21:48:10 +0530

    update Sidebar component with logo and improved structure

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import logo_full_dark from "../assets/images/logo_full_dark.svg";
 
 function Sidebar() {
   return (
-    <div className='w-64 bg-white'>Sidebar</div>
-  )
+    <aside className="w-64 bg-white">
+      <div className="flex flex-col items-center justify-center">
+        <img src={logo_full_dark} alt="logo_full_dark" />
+
+        <h1 className="text-2xl font-bold text-gray-800 mt-4">My App</h1>
+      </div>
+    </aside>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
